7bc86073c55cd94b5b8115fa7bb4a11e364fa03c
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2018-11-15 13:24:53 +0100

    Add HO once with call syntax, #9

--- Preparation/StepTwo/HO.hs
+++ Preparation/StepTwo/HO.hs
@@ -49,12 +49,14 @@ data (sig1 + sig2) (m :: * -> *) a = Inl (sig1 m a) | Inr (sig2 m a)
 
 infixr 0 +
 
-instance {-# OVERLAPPING #-} (Syntax sig1, Syntax sig2) => sig1 ⊂ (sig1 + sig2) where
+instance {-# OVERLAPPING #-}
+  (Syntax sig1, Syntax sig2) => sig1 ⊂ (sig1 + sig2) where
   inj = Inl
   prj (Inl fa) = Just fa
   prj _        = Nothing
 
-instance {-# OVERLAPPABLE #-} (Syntax sig1, sig ⊂ sig2) => sig ⊂ (sig1 + sig2) where
+instance {-# OVERLAPPABLE #-}
+  (Syntax sig1, sig ⊂ sig2) => sig ⊂ (sig1 + sig2) where
   inj = Inr . inj
   prj (Inr ga) = prj ga
   prj _        = Nothing
@@ -80,97 +82,9 @@ instance Functor sig => Syntax (Lift sig) where
   handle c hdl (Lift op) =
     Lift (fmap (\p -> hdl (fmap (const p) c)) op)
 
-data Nondet cnt = Fail' | cnt :|* cnt
-  deriving Functor
-
-type HNondet = Lift Nondet
-
 inject :: (sub ⊂ sup) => sub (Prog sup) a -> Prog sup a
 inject = Op . inj
 
 project :: (sub ⊂ sup) => Prog sup a -> Maybe (sub (Prog sup) a)
 project (Op s) = prj s
 project _      = Nothing
-
-pattern Fail <- (project -> Just (Lift Fail'))
-
-fail :: (HNondet ⊂ sig) => Prog sig a
-fail = inject $ Lift Fail'
-
-pattern p :|| q <- (project -> Just (Lift (p :|* q)))
-
-(||) :: (HNondet ⊂ sig) => Prog sig a -> Prog sig a -> Prog sig a
-p || q = inject $ Lift (p :|* q)
-
-pattern Other s = Op (Inr s)
-
-data Cut cnt = Cutfail'
-  deriving Functor
-
-type HCut = Lift Cut
-
-pattern Cutfail <- (project -> Just (Lift Cutfail'))
-
-cutfail :: (HCut ⊂ sig) => Prog sig a
-cutfail = inject $ Lift Cutfail'
-
-call :: forall sig a. (Syntax sig, HNondet ⊂ sig) => Prog (HCut + sig) a
-     -> Prog sig (Identity a)
-call p = go p fail
-  where
-    go :: Prog (HCut + sig) a -> Prog sig a -> Prog sig (Identity a)
-    go (Return a ) q = fmap Identity $ return a || q
-    go (Fail     ) q = fmap Identity $ q
-    go (Cutfail  ) q = fmap Identity $ fail
-    go (p1 :|| p2) q = go p1 (fmap runIdentity $ go p2 q)
-    go (Other op ) q = Op (handle (Identity ()) hdl op)
-
-    hdl :: (forall x. Identity (Prog (HCut + sig) x) -> Prog sig (Identity x))
-    hdl imx = call (runIdentity imx)
-
-cut :: (HNondet ⊂ sig, HCut ⊂ sig) => Prog sig ()
-cut = skip || cutfail
-
-skip :: Monad m => m ()
-skip = return ()
-
-once :: (HNondet ⊂ sig) => Prog (HCut + sig) b -> Prog sig b
-once p = fmap runIdentity $ call
-  (do
-    x <- p
-    cut
-    return x
-  )
-
-data Void cnt
-  deriving Functor
-
-type HVoid = Lift Void
-
-run :: Prog HVoid a -> a
-run (Return x) = x
-
-solutions :: forall sig a. (Syntax sig) => Prog (HNondet + sig) a
-          -> Prog sig [a]
-solutions (Return a) = return [a]
-solutions Fail       = return []
-solutions (p :|| q ) = liftM2 (++) (solutions p) (solutions q)
-solutions (Other op) = Op (handle [()] hdl op)
-  where hdl :: (forall x. [Prog (HNondet + sig) x] -> Prog sig [x])
-        hdl []     = return []
-        hdl (p:ps) = liftM2 (++) (solutions p) (hdl ps)
-
-e3 :: [[Int]]
-e3 = (run . solutions . once) (knapsack' 3 [3, 2, 1])
-
-knapsack' :: (HNondet ⊂ sig) => Int -> [Int] -> Prog sig [Int]
-knapsack' w vs
-  | w < 0 = fail
-  | w == 0 = return []
-  | w > 0 = do
-    v   <- select' vs
-    vs' <- knapsack' (w - v) vs
-    return (v : vs')
-
-select' :: (HNondet ⊂ sig) => [a] -> Prog sig a
-select' = foldr (||) fail . map return
