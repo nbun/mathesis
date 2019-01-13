1c2c707869a1a3a949df7a19f4e4dcce8f4ed17b
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2019-01-13 19:28:57 +0100

    Adapt container instance for sharing to combination-Functor, #33

--- Coq/src/Base.v
+++ Coq/src/Base.v
@@ -353,20 +353,23 @@ Section Sharing.
       from_to := from_to__Sharing
     }.
 
-  Fixpoint nameChoices (A : Type) (scope next : nat) (fs : Comb (Free C__Sharing) (Free C__Choice) A) : Free C__Choice A  :=
-    match fs with
-    | Inl _ _ _ (pure x) => pure x
-    | (Inl _ _ _ (impure (ext (ssharing n) pf))) => nameChoices n 1 (pf (psharing n))
-    end.
-
-  Fixpoint runSharing A  (fs : Comb (Free C__Sharing) (Free C__Choice) A) : Free C__Choice A :=
+  Fixpoint nameChoices (A : Type) (scope next : nat) (fs : Free (C__Comb C__Sharing C__Choice) A) : Free C__Choice A  :=
     match fs with
     | pure x => pure x
-    | impure (Inl _ _ _ (ext (ssharing n) pf)) => nameChoices i 1 n
-    | impure (ext (sput s') pf) => runState s' (pf (pput s'))
+    | impure (ext (inl (ssharing n)) pf) => nameChoices n 1 (pf (psharing n))
+    | impure (ext (inr sfail)        pf) => Fail A
+    | impure (ext (inr schoice)      pf) => let l := nameChoices scope (2 * next) (pf true) in
+                                           let r := nameChoices scope (2 * next + 1) (pf false)
+                                           in Choice' l r
     end.
 
-
+  Fixpoint runSharing A (fs : Free (C__Comb C__Sharing C__Choice) A) : Free C__Choice A :=
+    match fs with
+    | pure x => pure x
+    | impure (ext (inl (ssharing n)) pf) => nameChoices n 1 (pf (psharing n))
+    | impure (ext (inr sfail)         _) => Fail A
+    | impure (ext (inr schoice)      pf) => Choice' (runSharing (pf true)) (runSharing (pf false))
+    end.
 
 End Sharing.
 
