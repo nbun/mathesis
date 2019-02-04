f56076b72888eb9e6173120d8cc36a943ca682e5
Niels Bunkenburg <stu114713@informatik.uni-kiel.de>
2019-02-04 17:14:00 +0100

    Add induction hypothesis for Prog__SC, #49

--- Coq/src/Laws.v
+++ Coq/src/Laws.v
@@ -44,6 +44,36 @@ Section Free_Share_Ind.
   Defined.
 End Free_Share_Ind.
 
+Axiom Free_Share_dec__SC :
+  forall (A : Type) (p : Prog__SC A),
+    { exists x, p = pure x } +
+    { p = Fail__SC } +
+    { exists id p1 p2, p = Effect.Choice__SC id p1 p2}.
+
+Section Free_Share_Ind__SC.
+  Variable A : Type.
+  Variable P : Prog__SC A -> Prop.
+
+  Hypothesis HPure : forall (x : A), P (pure x).
+  Hypothesis HFail : P Fail__SC.
+  Hypothesis HChoice : forall id (p1 p2 : Prog__SC A), P p1 -> P p2 -> P (Effect.Choice__SC id p1 p2).
+
+  Lemma Free_Share_Ind__SC : forall (p : Prog__SC A), P p.
+  Proof.
+    intros p.
+    induction p using Free_Ind; eauto.
+    destruct (Free_Share_dec__SC (impure (ext s pf))) as [H1 | Hchoice].
+    - destruct H1 as [Hpure | Hfail].
+      + destruct Hpure as [x Hx]; discriminate Hx.
+      + rewrite Hfail; eauto.
+    - destruct Hchoice as [cid [p1 [p2 H12]]]; rewrite H12.
+      dependent destruction H12.
+      apply HChoice.
+      + apply (H true).
+      + apply (H false).
+  Defined.
+End Free_Share_Ind__SC.
+
 Fixpoint upcast__SC A (fc : Free (C__Comb C__Sharing C__Choice) A) : Prog A :=
   match fc with
   | pure x => pure x
@@ -148,7 +178,7 @@ Section SharingLaws.
   Theorem State_ND_independence : forall n m (p : Prog A),
       runState n p = runState m p.
   Proof.
-    intros n m p.
+    intros n m.
     induction p using Free_Share_Ind.
     - reflexivity.
     - reflexivity.
@@ -168,12 +198,22 @@ Section SharingLaws.
     reflexivity.
   Qed.
 
-  Lemma sharing_id_independence : forall (p : Free (C__Comb C__Sharing C__Choice) A) i j k,
-      upcast__C (nameChoices i j p) = upcast__C (nameChoices i k p).
+  Require Import Nat.
+  Search "dec".
+  Lemma sharing_id_independence : forall (p : Prog A) n i j k env1 env2,
+      (Search.dfs env1 (runChoice (nameChoices i j (runState n p)))) =
+      (Search.dfs env2 (runChoice (nameChoices i k (runState n p)))).
+      (* nameChoices i j p = nameChoices i k p. *)
   Proof.
-    intros p i j k.
-    induction p using Free_Share_Ind.
-      
+    induction p using Free_Share_Ind; intros n i j k env1 env2.
+    - reflexivity.
+    - reflexivity.
+    - simpl.
+      destruct (EqNat.eq_nat_decide i j) as [Heq|Hneq].
+      apply EqNat.eq_nat_eq in Heq.
+      subst.
+      Admitted.
+
   Theorem Choice__fstrict : forall (f' : A -> Prog A) p1 p2,
       Eq_Prog eqA (Share (p1 ? p2) >>= fun fx => fx >>= f') ((Share p1 ? Share p2) >>= fun fx => fx >>= f').
   Proof.
@@ -181,9 +221,25 @@ Section SharingLaws.
     unfold Eq_Prog, handle, Search.collectVals, Share'.
     simpl.
     repeat (rewrite nf_impure; simpl).
-Print nameChoices.    
-Print Free_Share_Ind.        
+    induction  (nf (p1 >>= (fun x : A => shareArgs x) >>= f')) using Free_Share_Ind;
+      induction  (nf (p2 >>= (fun x : A => shareArgs x) >>= f')) using Free_Share_Ind;
+      simpl; try reflexivity.
+    - econstructor; try reflexivity.
+
+
+    Admitted.
       
+
+  Theorem Choice__id : forall p1 p2,
+      Eq_Prog eqA (Share (p1 ? p2) >>= id) ((Share p1 ? Share p2) >>= id).
+  Proof.
+    intros.
+    unfold Eq_Prog, handle, Search.collectVals, Share', Fail.
+    simpl.
+    repeat (rewrite nf_impure; try unfold Share'; simpl).
+    constructor.
+
+    
 
 
 End SharingLaws.
